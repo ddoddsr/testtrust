e7e70793f339f26e47cee63f82d1539454ec5789
Dan Dodd <dan@foredev.com>
2023-04-24 10:07:04 -0500

    Initial companies install with socialite

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,18 +2,22 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\Company;
+use App\Models\ConnectedAccount;
+use App\Policies\CompanyPolicy;
+use App\Policies\ConnectedAccountPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The model to policy mappings for the application.
+     * The policy mappings for the application.
      *
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        Company::class => CompanyPolicy::class,
+        ConnectedAccount::class => ConnectedAccountPolicy::class,
     ];
 
     /**
@@ -21,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->registerPolicies();
+
         //
     }
 }
